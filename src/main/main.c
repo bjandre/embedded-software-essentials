8fab47ede9d17225812312c380649b06b2ee46e2
Ben Andre <bjandre@gmail.com>
2017-02-11 15:07:30 -0700

    Allow building executable without any project code.
    Change the logic in main to allow building an executable without any project
    code.
    
    Testing: pass
      macOS:
        make clean; make test
        make clean; make project=0 test
        make clean; make project=1 test
        make clean; make project=2 test
        make clean; make platform=frdm project=0 all
        make clean; make platform=frdm project=1 all
        make clean; make platform=frdm project=2 all

--- src/main/main.c
+++ src/main/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#if PROJECT == 1
+#if PROJECT == 0
+#    // NOTE(bja, 2017-02): Building main with no project code.
+#elif PROJECT == 1
 #    include "project_1.h"
 #else
-#    error Unsupported project number in PROJECT macro. Valid values: 1
+#    error "Unsupported project number in PROJECT macro. Valid values: 0, 1"
 #endif
 
 int main(int argc, char **argv)
