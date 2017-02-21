7c9f6621650c13b38e4eb4ac0f97028d80ac297a
Ben Andre <bjandre@gmail.com>
2017-02-21 00:45:45 -0700

    Adapt utils unit tests for project 2 requirements.
    Update the existing utils unit tests to have meaningful names, add tests as
    required by project 2 requirements.
    
    Testing:
      macOS: utils unit tests pass.

--- src/util/test_utils.c
+++ src/util/test_utils.c
@@ -13,35 +13,45 @@
 void compare_array_test(uint8_t *a, uint8_t *b, int length);
 
 // memory.h
-void test_my_memmove_1(void **state);
-void test_my_memmove_2(void **state);
-void test_my_memmove_3(void **state);
-void test_my_memmove_4(void **state);
-void test_my_memmove_5(void **state);
+void test_my_memmove_null_ptrs(void **state);
+void test_my_memmove_src_dest_complete_overlap(void **state);
+void test_my_memmove_no_overlap_src_dest(void **state);
+void test_my_memmove_no_overlap_dest_src(void **state);
+void test_my_memmove_overlap_src_in_dest(void **state);
+void test_my_memmove_overlap_dest_in_src(void **state);
 
-void test_my_memset_1(void **state);
-void test_my_memset_2(void **state);
-void test_my_memzero(void **state);
+void test_my_memset_null_ptrs(void **state);
+void test_my_memset_set_entire_array(void **state);
+void test_my_memset_set_subarray(void **state);
 
-void test_my_reverse_1(void **state);
-void test_my_reverse_2(void **state);
-void test_my_reverse_3(void **state);
-void test_my_reverse_4(void **state);
-void test_my_reverse_5(void **state);
-void test_my_reverse_6(void **state);
-void test_my_reverse_7(void **state);
+void test_my_memzero_null_ptrs(void **state);
+void test_my_memzero_set_entire_array(void **state);
+
+void test_my_reverse_null_ptr(void **state);
+void test_my_reverse_even_length(void **state);
+void test_my_reverse_odd_length(void **state);
+void test_my_reverse_zero_length(void **state);
+void test_my_reverse_subarray_after(void **state);
+void test_my_reverse_subarray_before(void **state);
+void test_my_reverse_all_characters(void **state);
 
 // data.h
+void test_big_to_little_null_ptr(void **state);
+void test_big_to_little_valid(void **state);
+
+void test_littl_to_big_null_ptr(void **state);
+void test_little_to_big_valid(void **state);
+
 void test_convert_endian32_1(void **state);
 void test_convert_endian32_2(void **state);
 
-void test_my_atoi_1(void **state);
-void test_my_atoi_2(void **state);
+void test_my_atoi_leading_whitespace_null_term(void **state);
+void test_my_atoi_negative_null_term(void **state);
 
-void test_my_itoa_1(void **state);
-void test_my_itoa_2(void **state);
-void test_my_itoa_3(void **state);
-void test_my_itoa_4(void **state);
+void test_my_itoa_positive_base10(void **state);
+void test_my_itoa_negative_base10(void **state);
+void test_my_itoa_positive_base2(void **state);
+void test_my_itoa_negative_base16(void **state);
 
 
 void compare_array_test(uint8_t *a, uint8_t *b, int length)
@@ -59,7 +69,24 @@ void compare_array_test(uint8_t *a, uint8_t *b, int length)
     }
 }
 
-void test_my_memmove_1(void **state)
+void test_my_memmove_null_ptrs(void **state)
+{
+    // test return status when null pointers are passed in
+#define SIZE 16
+
+    size_t const range = 4;
+    uint8_t data[SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    uint8_t *source = data + range;
+
+    MemStatus status = my_memmove(NULL, source, range);
+    assert_int_equal(status, MemStatus_ERROR_NULL);
+    status = my_memmove(source, NULL, range);
+    assert_int_equal(status, MemStatus_ERROR_NULL);
+
+#undef SIZE
+}
+
+void test_my_memmove_src_dest_complete_overlap(void **state)
 {
     // test complete overlap
 #define SIZE 16
@@ -80,7 +107,7 @@ void test_my_memmove_1(void **state)
 #undef RANGE
 }
 
-void test_my_memmove_2(void **state)
+void test_my_memmove_no_overlap_src_dest(void **state)
 {
     // test no overlap, source before destestination
 #define SIZE 16
@@ -101,7 +128,7 @@ void test_my_memmove_2(void **state)
 #undef RANGE
 }
 
-void test_my_memmove_3(void **state)
+void test_my_memmove_no_overlap_dest_src(void **state)
 {
     // test no overlap, destination before source
 #define SIZE 16
@@ -122,7 +149,7 @@ void test_my_memmove_3(void **state)
 #undef RANGE
 }
 
-void test_my_memmove_4(void **state)
+void test_my_memmove_overlap_src_in_dest(void **state)
 {
     // test overlap, destination before source, copy from begining of source
 #define SIZE 16
@@ -143,7 +170,7 @@ void test_my_memmove_4(void **state)
 #undef RANGE
 }
 
-void test_my_memmove_5(void **state)
+void test_my_memmove_overlap_dest_in_src(void **state)
 {
     // test overlap, source before destination, copy from end of source
 #define SIZE 16
@@ -164,16 +191,27 @@ void test_my_memmove_5(void **state)
 #undef RANGE
 }
 
-void test_my_memset_1(void **state)
+void test_my_memset_null_ptrs(void **state)
+{
+    // test return error when passed null pointer
+    size_t const size = 20;
+    uint8_t expected = 0x23;
+
+    MemStatus status = my_memset(NULL, size, expected);
+    assert_int_equal(status, MemStatus_ERROR_NULL);
+}
+
+void test_my_memset_set_entire_array(void **state)
 {
     // test setting entire array
     size_t const size = 20;
     uint8_t data[size];
     for (size_t i = 0; i < size; i++) {
-        *(data + i) = 0 + i;
+        *(data + i) = i;
     }
     uint8_t expected = 0x23;
     MemStatus status = my_memset(data, size, expected);
+    assert_int_equal(status, MemStatus_SUCCESS);
 
     for (size_t i = 0; i < size; i++) {
 #       ifdef NDEBUG
@@ -183,13 +221,9 @@ void test_my_memset_1(void **state)
 #       endif
         assert_int_equal(*(data + i), expected);
     }
-
-    status = my_memset(NULL, size, expected);
-    assert_int_not_equal(status, MemStatus_SUCCESS);
-
 }
 
-void test_my_memset_2(void **state)
+void test_my_memset_set_subarray(void **state)
 {
     // test a subset of an array to ensure we are not changing values outside
     // the specified range.
@@ -209,13 +243,22 @@ void test_my_memset_2(void **state)
 #undef RANGE
 }
 
-void test_my_memzero(void **state)
+void test_my_memzero_null_ptrs(void **state)
+{
+    // test error status when passed null pointer
+    uint32_t size = 10;
+    MemStatus status = my_memzero(NULL, size);
+    assert_int_equal(status, MemStatus_ERROR_NULL);
+
+}
+
+void test_my_memzero_set_entire_array(void **state)
 {
     // test zeroing an set of bytes
     size_t const size = 20;
     uint8_t data[size];
     for (size_t i = 0; i < size; i++) {
-        *(data + i) = 0 + i;
+        *(data + i) = i;
     }
 
     MemStatus status = my_memzero(data, size);
@@ -229,26 +272,17 @@ void test_my_memzero(void **state)
 #       endif
         assert_int_equal(*(data + i), expected);
     }
-
-    status = my_memzero(NULL, size);
-    assert_int_not_equal(status, MemStatus_SUCCESS);
-
 }
 
-void test_my_reverse_1(void **state)
+void test_my_reverse_null_ptr(void **state)
 {
-    // test reverse returns non-zero for null pointer.
-#define LENGTH 4
-    int length = LENGTH;
-    uint8_t data[LENGTH] = {0, 1, 2, 3};
-    uint8_t expected[LENGTH] = {0, 1, 2, 3};
+    // test reverse returns error for null pointer.
+    int length = 4;
     MemStatus status = my_reverse(NULL, length);
-    assert_int_not_equal(status, MemStatus_SUCCESS);
-    assert_memory_equal(expected, data, length);
-#undef LENGTH
+    assert_int_equal(status, MemStatus_ERROR_NULL);
 }
 
-void test_my_reverse_2(void **state)
+void test_my_reverse_even_length(void **state)
 {
     // test reverse returns reversed array for even length
 #define LENGTH 4
@@ -261,7 +295,7 @@ void test_my_reverse_2(void **state)
 #undef LENGTH
 }
 
-void test_my_reverse_3(void **state)
+void test_my_reverse_odd_length(void **state)
 {
     // test reverse returns reversed array for odd length
 #define LENGTH 5
@@ -274,7 +308,7 @@ void test_my_reverse_3(void **state)
 #undef LENGTH
 }
 
-void test_my_reverse_4(void **state)
+void test_my_reverse_zero_length(void **state)
 {
     // test reverse returns unmodified array for zero length
 #define LENGTH 5
@@ -287,7 +321,7 @@ void test_my_reverse_4(void **state)
 #undef LENGTH
 }
 
-void test_my_reverse_5(void **state)
+void test_my_reverse_subarray_after(void **state)
 {
     // test reverse does not affect memory beyond specified length.
 #define LENGTH 5
@@ -300,7 +334,7 @@ void test_my_reverse_5(void **state)
 #undef LENGTH
 }
 
-void test_my_reverse_6(void **state)
+void test_my_reverse_subarray_before(void **state)
 {
     // test reverse does not affect memory before the specified start
 #define LENGTH 5
@@ -313,10 +347,10 @@ void test_my_reverse_6(void **state)
 #undef LENGTH
 }
 
-void test_my_reverse_7(void **state)
+void test_my_reverse_all_characters(void **state)
 {
     // test reversing the entire array, even length
-    size_t const size = 20;
+    size_t const size = 256;
     uint8_t data[size];
     uint8_t expected[size];
     for (size_t i = 0; i < size; i++) {
@@ -328,6 +362,51 @@ void test_my_reverse_7(void **state)
     assert_int_equal(status, MemStatus_SUCCESS);
     assert_memory_equal(expected, data, size);
 
+}
+
+void test_big_to_little_null_ptr(void **state)
+{
+    // test convert big to little error with null pointer
+    uint32_t const length = 1;
+
+    DataStatus status = big_to_little32(NULL, length);
+    assert_int_equal(status, DataStatus_ERROR_NULL);
+}
+
+void test_big_to_little_valid(void **state)
+{
+    // test convert endian by round tripping data, single 8-byte int
+    uint32_t const length = 1;
+    uint32_t data = 0x01234567;
+    uint32_t expected = 0x67452301;
+    size_t num_bytes = sizeof(data);
+
+    DataStatus status = big_to_little32(&data, length);
+    assert_int_equal(status, DataStatus_SUCCESS);
+    assert_memory_equal((uint8_t *)&expected, (uint8_t *)&data, length * num_bytes);
+}
+
+void test_little_to_big_null_ptr(void **state)
+{
+    // test convert big to little error with null pointer
+    uint32_t const length = 1;
+
+    DataStatus status = big_to_little32(NULL, length);
+    assert_int_equal(status, DataStatus_ERROR_NULL);
+}
+
+void test_little_to_big_valid(void **state)
+{
+    // test convert endian by round tripping data, single 8-byte int
+    uint32_t const length = 1;
+    uint32_t data = 0x67452301;
+    uint32_t expected = 0x01234567;
+    size_t num_bytes = sizeof(data);
+
+    DataStatus status = little_to_big32(&data, length);
+    assert_int_equal(status, DataStatus_SUCCESS);
+    assert_memory_equal((uint8_t *)&expected, (uint8_t *)&data,
+                       length * num_bytes);
 }
 
 void test_convert_endian32_1(void **state)
@@ -384,7 +463,7 @@ void test_convert_endian32_2(void **state)
 
 }
 
-void test_my_atoi_1(void **state)
+void test_my_atoi_leading_whitespace_null_term(void **state)
 {
     // test converting a null terminated string with leading whitespace.
 #define LENGTH 10
@@ -400,7 +479,7 @@ void test_my_atoi_1(void **state)
 #undef LENGTH
 }
 
-void test_my_atoi_2(void **state)
+void test_my_atoi_negative_null_term(void **state)
 {
     // test converting a negative number as a null terminated string
 #define LENGTH 10
@@ -416,7 +495,7 @@ void test_my_atoi_2(void **state)
 #undef LENGTH
 }
 
-void test_my_itoa_1(void **state)
+void test_my_itoa_positive_base10(void **state)
 {
     // test converting an integer base 10 to a string
 #define LENGTH 33
@@ -437,7 +516,7 @@ void test_my_itoa_1(void **state)
 #undef LENGTH
 }
 
-void test_my_itoa_2(void **state)
+void test_my_itoa_negative_base10(void **state)
 {
     // test converting an negative integer base 10 to a string
 #define LENGTH 33
@@ -459,7 +538,7 @@ void test_my_itoa_2(void **state)
 }
 
 
-void test_my_itoa_3(void **state)
+void test_my_itoa_positive_base2(void **state)
 {
     // test converting an integer base 2 to a string
 #define LENGTH 33
@@ -481,7 +560,7 @@ void test_my_itoa_3(void **state)
 #undef LENGTH
 }
 
-void test_my_itoa_4(void **state)
+void test_my_itoa_negative_base16(void **state)
 {
     // test converting a negative integer base 16 to a string
 #define LENGTH 33
@@ -506,29 +585,36 @@ void test_my_itoa_4(void **state)
 int main(int argc, char **argv)
 {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(test_my_memmove_1),
-        cmocka_unit_test(test_my_memmove_2),
-        cmocka_unit_test(test_my_memmove_3),
-        cmocka_unit_test(test_my_memmove_4),
-        cmocka_unit_test(test_my_memmove_5),
-        cmocka_unit_test(test_my_memset_1),
-        cmocka_unit_test(test_my_memset_2),
-        cmocka_unit_test(test_my_memzero),
-        cmocka_unit_test(test_my_reverse_1),
-        cmocka_unit_test(test_my_reverse_2),
-        cmocka_unit_test(test_my_reverse_3),
-        cmocka_unit_test(test_my_reverse_4),
-        cmocka_unit_test(test_my_reverse_5),
-        cmocka_unit_test(test_my_reverse_6),
-        cmocka_unit_test(test_my_reverse_7),
+        cmocka_unit_test(test_my_memmove_null_ptrs),
+        cmocka_unit_test(test_my_memmove_src_dest_complete_overlap),
+        cmocka_unit_test(test_my_memmove_no_overlap_src_dest),
+        cmocka_unit_test(test_my_memmove_no_overlap_dest_src),
+        cmocka_unit_test(test_my_memmove_overlap_src_in_dest),
+        cmocka_unit_test(test_my_memmove_overlap_dest_in_src),
+        cmocka_unit_test(test_my_memset_null_ptrs),
+        cmocka_unit_test(test_my_memset_set_entire_array),
+        cmocka_unit_test(test_my_memset_set_subarray),
+        cmocka_unit_test(test_my_memzero_null_ptrs),
+        cmocka_unit_test(test_my_memzero_set_entire_array),
+        cmocka_unit_test(test_my_reverse_null_ptr),
+        cmocka_unit_test(test_my_reverse_even_length),
+        cmocka_unit_test(test_my_reverse_odd_length),
+        cmocka_unit_test(test_my_reverse_zero_length),
+        cmocka_unit_test(test_my_reverse_subarray_after),
+        cmocka_unit_test(test_my_reverse_subarray_before),
+        cmocka_unit_test(test_my_reverse_all_characters),
+        cmocka_unit_test(test_big_to_little_null_ptr),
+        cmocka_unit_test(test_big_to_little_valid),
+        cmocka_unit_test(test_little_to_big_null_ptr),
+        cmocka_unit_test(test_little_to_big_valid),
         cmocka_unit_test(test_convert_endian32_1),
         cmocka_unit_test(test_convert_endian32_2),
-        cmocka_unit_test(test_my_atoi_1),
-        cmocka_unit_test(test_my_atoi_2),
-        cmocka_unit_test(test_my_itoa_1),
-        cmocka_unit_test(test_my_itoa_2),
-        cmocka_unit_test(test_my_itoa_3),
-        cmocka_unit_test(test_my_itoa_4),
+        cmocka_unit_test(test_my_atoi_leading_whitespace_null_term),
+        cmocka_unit_test(test_my_atoi_negative_null_term),
+        cmocka_unit_test(test_my_itoa_positive_base10),
+        cmocka_unit_test(test_my_itoa_negative_base10),
+        cmocka_unit_test(test_my_itoa_positive_base2),
+        cmocka_unit_test(test_my_itoa_negative_base16),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
