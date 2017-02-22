bf536ba14c9865af92c2c63f6597f72ef06cc1cd
Ben Andre <bjandre@gmail.com>
2017-02-21 21:52:47 -0700

    Update default build to be project 2.
    Testing:
      macOS: all unit tests pass, main runs.

--- makerules/commandline.makefile
+++ makerules/commandline.makefile
@@ -8,7 +8,7 @@ project = unset
 # define the project macro controlling the build code.
 strip_project := $(strip $(project))
 ifeq ($(strip_project), unset)
-  PROJECT := 1
+  PROJECT := 2
 else 
   PROJECT := $(strip_project)
 endif
--- src/main/main.c
+++ src/main/main.c
@@ -5,6 +5,8 @@
 #    // NOTE(bja, 2017-02): Building main with no project code.
 #elif PROJECT == 1
 #    include "project_1.h"
+#elif PROJECT == 2
+#
 #else
 #    error "Unsupported project number in PROJECT macro. Valid values: 0, 1"
 #endif
@@ -15,6 +17,8 @@ int main(int argc, char **argv)
 
 #   if PROJECT == 1
     project_1_report();
+#   elif PROJECT == 2
+#
 #   endif
 
     return 0;
