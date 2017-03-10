bbed7827989367fd7220b192333465f383a98e8b
Ben Andre <bjandre@gmail.com>
2017-03-09 21:07:29 -0700

    Bugfix in bit-operations
    Bugfix in the assertion inequalities, < should be <= to ensure max possible
    shift. Add function doc to header file.
    
    Testing:
    
      macOS - unit tests pass.

--- src/util/bit-operations.h
+++ src/util/bit-operations.h
@@ -8,32 +8,80 @@
 
 /**
 
+   MAX_SHIFT()
+
+   Determine the maximum valid bit shift for a particular variable.
+
+   Param: X a any valid type.
+
  */
 #define MAX_SHIFT(X) (sizeof((X)) * CHAR_BIT - 1)
 
+/**
+
+   set_bit_one()
+
+   Set the bit in the specified position to one.
+
+   Parameters: current - pointer to the bitfield to be modified
+               shift - position of the bit to modify
+
+ */
 inline void set_bit_one(volatile uint32_t *current, uint8_t shift)
 {
-    assert(shift < MAX_SHIFT(*current));
+    assert(shift <= MAX_SHIFT(*current));
     *current |= (1 << shift);
 }
 
+/**
+
+   set_bit_zero()
+
+   Set the bit in the specified position to zero.
+
+   Parameters: current - pointer to the bitfield to be modified
+               shift - position of the bit to modify
+
+ */
 inline void set_bit_zero(volatile uint32_t *current, uint8_t shift)
 {
-    assert(shift < MAX_SHIFT(*current));
+    assert(shift <= MAX_SHIFT(*current));
     *current &= ~(1 << shift);
 }
 
+/**
+
+   flip_bit()
+
+   Flip the bit in the specified position.
+
+   Parameters: current - pointer to the bitfield to be modified
+               shift - position of the bit to modify
+
+ */
 inline void flip_bit(volatile uint32_t *current, uint8_t shift)
 {
-    assert(shift < MAX_SHIFT(*current));
+    assert(shift <= MAX_SHIFT(*current));
     *current ^= (1 << shift);
 }
 
+/**
+
+   get_bit()
+
+   Return the boolean value of the bit in the specified position.
+
+   Parameters: current - pointer to the bitfield to be modified
+               shift - position of the bit to modify
+               value - pointer to the memory for the return value
+
+ */
 inline void get_bit(volatile uint32_t *current, uint8_t shift, bool *value)
 {
-    assert(shift < MAX_SHIFT(*current));
+    assert(shift <= MAX_SHIFT(*current));
     *value = *current & (1 << shift);
 }
 
+#undef MAX_SHIFT
 
 #endif // ESE_UTIL_BIT_OPTS_H_
