5c98923a4d2b666d7e3b03ccad9ae98c41696471
Ben Andre <bjandre@gmail.com>
2017-04-17 13:39:20 -0600

    Bugfix work arounds and refactoring for profiling.
    Workaround for occasional timer overflow miss by manually incrementing if
    necessary. Refactor memory profiling to breakup tasks into standalone
    independent functions, allocate large block of memory once and reuse for all
    tests to avoid heap problems?
    
    Dies at runtime for 5000 byte transfer with dma bus error on source with about
    100 bytes remaining to transfer....?
    
    Testing:
      fedora x KDS - runs, see above.
      fedora x frdm - runs, see above.
    
      fedora - debug build runs to completion, minor profiling time differences as
               expected.

--- src/app/memory-profile.c
+++ src/app/memory-profile.c
@@ -27,12 +27,12 @@ const uint8_t num_trials = 100;
 
 /**
  */
-void profile_memmove(log_item_t *item, uint32_t num_bytes);
+void profile_memmove(log_item_t *item, uint32_t num_bytes, uint8_t *destination, uint8_t *source);
 
 /**
 
  */
-void profile_memset(log_item_t *item, uint32_t num_bytes);
+void profile_memset(log_item_t *item, uint32_t num_bytes, uint8_t *destination, uint8_t source);
 
 void memory_profile(log_item_t *item)
 {
@@ -56,6 +56,10 @@ void memory_profile(log_item_t *item)
     const size_t num_bytes = NUM_BYTES;
     uint16_t bytes[NUM_BYTES] = {12u, 100u, 1000u, 5000u};
 #undef NUM_BYTES
+    uint8_t *destination = (uint8_t *)malloc(bytes[num_bytes-1]);
+    uint8_t *source = (uint8_t *)malloc(bytes[num_bytes-1]);
+    uint8_t cp_value = 0xAA;
+
 
     const char name_total_bytes[] = "total_bytes";
     for (size_t nb = 0; nb < num_bytes; nb++) {
@@ -65,170 +69,207 @@ void memory_profile(log_item_t *item)
         UpdateLogItem(item, PROFILING_VALUE, sizeof(uint16_t), bytes + nb);
         log_item(item);
 
-        profile_memset(item, bytes[nb]);
-        profile_memmove(item, bytes[nb]);
+        profile_memset(item, bytes[nb], destination, cp_value);
+        profile_memmove(item, bytes[nb], destination, source);
 
         UpdateLogItem(item, PROFILING_END, sizeof(name_total_bytes), &name_total_bytes);
         log_item(item);
     }
 
+    free(destination);
+    free(source);
+
     enable_global_async_profiling_active();
 }
 
-
-void profile_memmove(log_item_t *item, uint32_t num_bytes)
-{
-    uint8_t *destination = malloc(num_bytes);
-    uint8_t *source = malloc(num_bytes);
+void profile_memmov_sys(uint32_t num_bytes, log_item_t* item,
+		uint8_t* destination, uint8_t* source) {
     profiling_timer_data_t start_time, end_time;
-    uint32_t total_clocks;
-    uint32_t average_clocks;
-
-    total_clocks = 0;
-
-    const char name_memmove_stdlib[] = "memmove_stdlib";
-    UpdateLogItem(item, PROFILING_START, sizeof(name_memmove_stdlib),
-                  &name_memmove_stdlib);
-    log_item(item);
-    for (uint8_t i = 0; i < num_trials; i++) {
-        get_timer(&start_time);
-        memmove(destination, source, num_bytes);
-        get_timer(&end_time);
-        total_clocks += elapsed_time(&start_time, &end_time);
-    }
-    average_clocks = total_clocks / num_trials;
-    UpdateLogItem(item, PROFILING_VALUE, sizeof(uint32_t), &average_clocks);
-    log_item(item);
-    UpdateLogItem(item, PROFILING_END, sizeof(name_memmove_stdlib),
-                  &name_memmove_stdlib);
-    log_item(item);
-
-    total_clocks = 0;
-
-    const char name_memmove_cpu[] = "memmove_cpu";
-    UpdateLogItem(item, PROFILING_START, sizeof(name_memmove_cpu),
-                  &name_memmove_cpu);
-    log_item(item);
-    for (uint8_t i = 0; i < num_trials; i++) {
-        get_timer(&start_time);
-        memmove_cpu(destination, source, num_bytes);
-        get_timer(&end_time);
-        total_clocks += elapsed_time(&start_time, &end_time);
-    }
-    average_clocks = total_clocks / num_trials;
-    UpdateLogItem(item, PROFILING_VALUE, sizeof(uint32_t), &average_clocks);
-    log_item(item);
-    UpdateLogItem(item, PROFILING_END, sizeof(name_memmove_cpu), &name_memmove_cpu);
-    log_item(item);
-
-#define NUM_BYTES_PER_TRANSFER 3
-    const uint32_t num_bytes_per_transfer = NUM_BYTES_PER_TRANSFER;
-    uint32_t bytes_per_transfer[NUM_BYTES_PER_TRANSFER] = {1, 2, 4};
-#undef NUM_BYTES_PER_TRANSFER
-
-    const char name_memmove_dma[] = "memmove_dma";
-    UpdateLogItem(item, PROFILING_START, sizeof(name_memmove_dma),
-                  &name_memmove_dma);
-    log_item(item);
-    for (size_t nbpt = 0; nbpt < num_bytes_per_transfer; nbpt++) {
-        total_clocks = 0;
-        char str_nbpt = '0' + bytes_per_transfer[nbpt];
-        UpdateLogItem(item, PROFILING_START, sizeof(char), &str_nbpt);
-        log_item(item);
-        for (uint8_t i = 0; i < num_trials; i++) {
-            bool dma_complete = false;
-            get_timer(&start_time);
-            memmove_dma(destination, source, num_bytes, bytes_per_transfer[nbpt]);
-            // wait for dma to finish and set end time in interrupt!
-            while (!dma_complete) {
-                dma_complete = get_global_async_dma_complete();
-            }
-            end_time = get_global_async_profiling_end_time();
-            total_clocks += elapsed_time(&start_time, &end_time);
-        }
-        average_clocks = total_clocks / num_trials;
-        UpdateLogItem(item, PROFILING_VALUE, sizeof(uint32_t), &average_clocks);
-        log_item(item);
-        UpdateLogItem(item, PROFILING_END, sizeof(char), &str_nbpt);
-        log_item(item);
-    }
-    UpdateLogItem(item, PROFILING_END, sizeof(name_memmove_dma), &name_memmove_dma);
-    log_item(item);
-
-    free(destination);
-    free(source);
+	uint32_t total_clocks;
+	uint32_t average_clocks;
+	total_clocks = 0;
+	const char name_memmove_stdlib[] = "memmove_stdlib";
+	UpdateLogItem(item, PROFILING_START, sizeof(name_memmove_stdlib),
+			&name_memmove_stdlib);
+	log_item(item);
+	for (uint8_t i = 0; i < num_trials; i++) {
+		get_timer(&start_time);
+		memmove(destination, source, num_bytes);
+		get_timer(&end_time);
+		total_clocks += elapsed_time(&start_time, &end_time);
+	}
+	average_clocks = total_clocks / num_trials;
+	UpdateLogItem(item, PROFILING_VALUE, sizeof(uint32_t), &average_clocks);
+	log_item(item);
+	UpdateLogItem(item, PROFILING_END, sizeof(name_memmove_stdlib),
+			&name_memmove_stdlib);
+	log_item(item);
 }
 
-void profile_memset(log_item_t *item, uint32_t num_bytes)
+void profile_memmov_cpu(uint32_t num_bytes, log_item_t* item, uint8_t* destination,
+		uint8_t* source) {
+	profiling_timer_data_t start_time, end_time;
+	uint32_t total_clocks;
+	uint32_t average_clocks;
+
+	total_clocks = 0;
+	const char name_memmove_cpu[] = "memmove_cpu";
+	UpdateLogItem(item, PROFILING_START, sizeof(name_memmove_cpu),
+			&name_memmove_cpu);
+	log_item(item);
+	for (uint8_t i = 0; i < num_trials; i++) {
+		get_timer(&start_time);
+		memmove_cpu(destination, source, num_bytes);
+		get_timer(&end_time);
+		total_clocks += elapsed_time(&start_time, &end_time);
+	}
+	average_clocks = total_clocks / num_trials;
+	UpdateLogItem(item, PROFILING_VALUE, sizeof(uint32_t), &average_clocks);
+	log_item(item);
+	UpdateLogItem(item, PROFILING_END, sizeof(name_memmove_cpu),
+			&name_memmove_cpu);
+	log_item(item);
+}
+
+void profile_memmov_dma(uint32_t num_bytes,
+		log_item_t* item, uint8_t* destination, uint8_t* source) {
+#define NUM_BYTES_PER_TRANSFER 3
+	const uint32_t num_bytes_per_transfer = NUM_BYTES_PER_TRANSFER;
+	uint32_t bytes_per_transfer[NUM_BYTES_PER_TRANSFER] = { 1, 2, 4 };
+#undef NUM_BYTES_PER_TRANSFER
+	profiling_timer_data_t start_time, end_time;
+	uint32_t total_clocks;
+	uint32_t average_clocks;
+
+	const char name_memmove_dma[] = "memmove_dma";
+	UpdateLogItem(item, PROFILING_START, sizeof(name_memmove_dma),
+			&name_memmove_dma);
+	log_item(item);
+	for (size_t nbpt = 0; nbpt < num_bytes_per_transfer; nbpt++) {
+		total_clocks = 0;
+		char str_nbpt = '0' + bytes_per_transfer[nbpt];
+		UpdateLogItem(item, PROFILING_START, sizeof(char), &str_nbpt);
+		log_item(item);
+		for (uint8_t i = 0; i < num_trials; i++) {
+			bool dma_complete = false;
+			get_timer(&start_time);
+			memmove_dma(destination, source, num_bytes,
+					bytes_per_transfer[nbpt]);
+			// wait for dma to finish and set end time in interrupt!
+			while (!dma_complete) {
+				dma_complete = get_global_async_dma_complete();
+			}
+			end_time = get_global_async_profiling_end_time();
+			total_clocks += elapsed_time(&start_time, &end_time);
+		}
+		average_clocks = total_clocks / num_trials;
+		UpdateLogItem(item, PROFILING_VALUE, sizeof(uint32_t), &average_clocks);
+		log_item(item);
+		UpdateLogItem(item, PROFILING_END, sizeof(char), &str_nbpt);
+		log_item(item);
+	}
+	UpdateLogItem(item, PROFILING_END, sizeof(name_memmove_dma),
+			&name_memmove_dma);
+	log_item(item);
+}
+
+void profile_memmove(log_item_t *item, uint32_t num_bytes, uint8_t *destination, uint8_t *source)
 {
-    uint8_t *destination = malloc(num_bytes);
-    uint8_t source = 0xAA;
+	profile_memmov_sys(num_bytes, item, destination, source);
+	profile_memmov_cpu(num_bytes, item, destination, source);
+	profile_memmov_dma(num_bytes, item, destination, source);
+
+}
+
+void profile_memset_system(uint8_t source, uint32_t num_bytes,
+		log_item_t* item, uint8_t* destination) {
     profiling_timer_data_t start_time, end_time;
     uint32_t total_clocks = 0;
     uint32_t average_clocks = 0;
-
     total_clocks = 0;
+	const char name_memset_stdlib[] = "memset_stdlib";
+	UpdateLogItem(item, PROFILING_START, sizeof(name_memset_stdlib),
+			&name_memset_stdlib);
+	log_item(item);
+	for (uint8_t i = 0; i < num_trials; i++) {
+		get_timer(&start_time);
+		memset(destination, source, num_bytes);
+		get_timer(&end_time);
+		total_clocks += elapsed_time(&start_time, &end_time);
+	}
+	average_clocks = total_clocks / num_trials;
+	UpdateLogItem(item, PROFILING_VALUE, sizeof(uint32_t), &average_clocks);
+	log_item(item);
+	UpdateLogItem(item, PROFILING_END, sizeof(name_memset_stdlib),
+			&name_memset_stdlib);
+	log_item(item);
+}
 
-    const char name_memset_stdlib[] = "memset_stdlib";
-    UpdateLogItem(item, PROFILING_START, sizeof(name_memset_stdlib),
-                  &name_memset_stdlib);
-    log_item(item);
-    for (uint8_t i = 0; i < num_trials; i++) {
-        get_timer(&start_time);
-        memset(destination, source, num_bytes);
-        get_timer(&end_time);
-        total_clocks += elapsed_time(&start_time, &end_time);
-    }
-    average_clocks = total_clocks / num_trials;
-    UpdateLogItem(item, PROFILING_VALUE, sizeof(uint32_t), &average_clocks);
-    log_item(item);
-    UpdateLogItem(item, PROFILING_END, sizeof(name_memset_stdlib),
-                  &name_memset_stdlib);
-    log_item(item);
+void profile_memset_cpu(uint8_t source, uint32_t num_bytes,
+		log_item_t* item, uint8_t* destination) {
 
+	profiling_timer_data_t start_time, end_time;
+    uint32_t total_clocks = 0;
+    uint32_t average_clocks = 0;
     total_clocks = 0;
 
     const char name_memset_cpu[] = "memset_cpu";
-    UpdateLogItem(item, PROFILING_START, sizeof(name_memset_cpu), &name_memset_cpu);
-    log_item(item);
-    for (uint8_t i = 0; i < num_trials; i++) {
-        get_timer(&start_time);
-        memset_cpu(destination, &source, num_bytes);
-        get_timer(&end_time);
-        total_clocks += elapsed_time(&start_time, &end_time);
-    }
-    average_clocks = total_clocks / num_trials;
-    UpdateLogItem(item, PROFILING_VALUE, sizeof(uint32_t), &average_clocks);
-    log_item(item);
-    UpdateLogItem(item, PROFILING_END, sizeof(name_memset_cpu), &name_memset_cpu);
-    log_item(item);
+	UpdateLogItem(item, PROFILING_START, sizeof(name_memset_cpu),
+			&name_memset_cpu);
+	log_item(item);
+	for (uint8_t i = 0; i < num_trials; i++) {
+		get_timer(&start_time);
+		memset_cpu(destination, &source, num_bytes);
+		get_timer(&end_time);
+		total_clocks += elapsed_time(&start_time, &end_time);
+	}
+	average_clocks = total_clocks / num_trials;
+	UpdateLogItem(item, PROFILING_VALUE, sizeof(uint32_t), &average_clocks);
+	log_item(item);
+	UpdateLogItem(item, PROFILING_END, sizeof(name_memset_cpu),
+			&name_memset_cpu);
+	log_item(item);
+}
 
-    /**
-       NOTE(bja, 2017-04) by definition in the standard library, memset operates on
-       single bytes. Therefore, it doesn't make sense or provide a fair comparison to
-       profile multi-byte DMA transfer for memset.
-     */
-    total_clocks = 0;
+void profile_memset_dma(uint8_t source,
+		uint32_t num_bytes, log_item_t* item, uint8_t* destination) {
 
-    const char name_memset_dma[] = "memset_dma";
-    UpdateLogItem(item, PROFILING_START, sizeof(name_memset_dma), &name_memset_dma);
-    log_item(item);
-    for (uint8_t i = 0; i < num_trials; i++) {
-        bool dma_complete = false;
-        get_timer(&start_time);
-        memset_dma(destination, &source, num_bytes, sizeof(uint8_t));
-        // wait for dma to finish and set end time in interrupt!
-        while (!dma_complete) {
-            dma_complete = get_global_async_dma_complete();
-        }
-        end_time = get_global_async_profiling_end_time();
-        total_clocks += elapsed_time(&start_time, &end_time);
-    }
-    average_clocks = total_clocks / num_trials;
-    UpdateLogItem(item, PROFILING_VALUE, sizeof(uint32_t), &average_clocks);
-    log_item(item);
-    UpdateLogItem(item, PROFILING_END, sizeof(name_memset_dma), &name_memset_dma);
-    log_item(item);
+	profiling_timer_data_t start_time, end_time;
+	uint32_t total_clocks = 0;
+	uint32_t average_clocks = 0;
+	total_clocks = 0;
+	/**
+	 NOTE(bja, 2017-04) by definition in the standard library, memset operates on
+	 single bytes. Therefore, it doesn't make sense or provide a fair comparison to
+	 profile multi-byte DMA transfer for memset.
+	 */
+	total_clocks = 0;
+	const char name_memset_dma[] = "memset_dma";
+	UpdateLogItem(item, PROFILING_START, sizeof(name_memset_dma),
+			&name_memset_dma);
+	log_item(item);
+	for (uint8_t i = 0; i < num_trials; i++) {
+		volatile bool dma_complete = false;
+		get_timer(&start_time);
+		memset_dma(destination, &source, num_bytes, sizeof(uint8_t));
+		// wait for dma to finish and set end time in interrupt!
+		while (!dma_complete) {
+			dma_complete = get_global_async_dma_complete();
+		}
+		end_time = get_global_async_profiling_end_time();
+		total_clocks += elapsed_time(&start_time, &end_time);
+	}
+	average_clocks = total_clocks / num_trials;
+	UpdateLogItem(item, PROFILING_VALUE, sizeof(uint32_t), &average_clocks);
+	log_item(item);
+	UpdateLogItem(item, PROFILING_END, sizeof(name_memset_dma),
+			&name_memset_dma);
+	log_item(item);
+}
 
-    free(destination);
+void profile_memset(log_item_t *item, uint32_t num_bytes, uint8_t *destination, uint8_t source)
+{
+	profile_memset_system(source, num_bytes, item, destination);
+	profile_memset_cpu(source, num_bytes, item, destination);
+	profile_memset_dma(source, num_bytes, item, destination);
 }
--- src/app/profiler.c
+++ src/app/profiler.c
@@ -9,9 +9,12 @@
 */
 #include <assert.h>
 #include <limits.h>
+#include <stdbool.h>
 
 #include "profiling-timer.h"
 #include "profiler.h"
+
+#include "async-global.h"
 
 void initialize_profiler(void)
 {
@@ -30,6 +33,17 @@ uint32_t elapsed_time(profiling_timer_data_t const *const start_time,
     int32_t delta_overflow = end_time->overflow_count - start_time->overflow_count;
     delta_time += delta_overflow * end_time->max_timer_value;
     // NOTE(bja, 2017-04) trap signed/unsigned conversion overflow problem
+    if (delta_time > 40000000u) {
+        // missed a overflow somehow....
+        increment_global_async_profiling_overflow();
+        delta_time = end_time->timer_count - start_time->timer_count;
+        delta_overflow = 1+end_time->overflow_count - start_time->overflow_count;
+        delta_time += delta_overflow * end_time->max_timer_value;
+        if (delta_time > 40000000u) {
+            bool inf_loop = true;
+            while(inf_loop){}
+        }
+    }
     assert(delta_time < 40000000u);
     return (uint32_t)delta_time;
 }
